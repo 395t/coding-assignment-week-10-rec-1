dcad1f2b5ad116c128704166961a282db5ba3ee4
Jie Hao Liao <liaojh1998@gmail.com>
2021-10-26 09:19:22 -0500

    Added testing

--- src/yolo/test.py
+++ src/yolo/test.py
@@ -21,7 +21,7 @@ def voc_eval(args):
                            image_sets=[(f'{args.year}', args.set)],
                            transform=valid_transform,
                            dataset_name=f'voc{args.year}')
-    model = Yolov1(backbone_name=args.backbone, model_save_dir=f'models/{args.name}')
+    model = Yolov1(backbone_name=args.backbone, model_save_dir=f'models/voc{args.year}_{args.backbone}')
     optimizer = SGD(model.parameters(),
                     lr=1e-3,
                     momentum=0.9,
@@ -31,7 +31,7 @@ def voc_eval(args):
                                   gamma=0.1,
                                   warm_up_factor=0.1,
                                   warm_up_iters=1)
-    _ = model.load_model(optimizer=optimizer, lr_scheduler=scheduler)
+    _ = model.load_model(optimizer=optimizer, lr_scheduler=scheduler, step=args.step)
 
     model.eval()
     mean_ap, ap_dict = calc_map(dataset, VOC_CLASSES, model, valid_transform)
@@ -44,6 +44,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Single Shot MultiBox Detector Training With Pytorch Evaluation')
     parser.add_argument('--set', default='test', choices=['test', 'train', 'valid'], help='dataset to evaluate on')
     parser.add_argument('--year', default=2007, type=int, help='dataset year')
-    parser.add_argument('--name')
     parser.add_argument('--backbone', default='yolo', choices=['yolo', 'resnet18', 'resnet50', 'vgg11', 'vgg16'])
-    parser.add_argument('--epoch', default=None)
+    parser.add_argument('--step', default=None)
+
+    args = parser.parse_args()
+    voc_eval(args)
